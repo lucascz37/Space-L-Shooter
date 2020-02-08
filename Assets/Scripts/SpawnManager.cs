17c5e3a1c04869fe10b64e882bc16e7d1ddf53ca
lucascz37 <lucascz37@gmail.com>
2020-02-07 22:42:18 -0300

    Spawn Manager
    Adição de Spawn Manager.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemy;
+    void Start()
+    {
+        StartCoroutine("SpawnRoutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(_enemy, new Vector3(Random.Range(-8f, 8f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
